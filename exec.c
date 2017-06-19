78934fc25474d1f7e77336116ac5c5ca3969acfa
Dr. David Alan Gilbert <dgilbert@redhat.com>
2017-06-19 16:11:02 +0100

    qemu_ram_block_host_offset
    Utility to give the offset of a host pointer within a RAMBlock
    (assuming we already know it's in that RAMBlock)
    
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- exec.c
+++ exec.c
@@ -2218,6 +2218,12 @@ static void *qemu_ram_ptr_length(RAMBlock *ram_block, ram_addr_t addr,
     return ramblock_ptr(block, addr);
 }
 
+/* Return the offset of a hostpointer within a ramblock */
+ram_addr_t qemu_ram_block_host_offset(RAMBlock *rb, void *host)
+{
+    return (uint8_t *)host - (uint8_t *)rb->host;
+}
+
 /*
  * Translates a host ptr back to a RAMBlock, a ram_addr and an offset
  * in that RAMBlock.
